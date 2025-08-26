428f76ac963358ac825480e910000ebf9311d133
kezagiselle <kezagiselle776@gmail.com>
2025-08-26 11:08:00 +0200

    changes

--- dropOut/src/components/Hod.tsx
+++ dropOut/src/components/Hod.tsx
@@ -13,8 +13,9 @@ import { IoIosPeople } from "react-icons/io";
 import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 import { FaCalendarAlt } from "react-icons/fa";
 import { FaClipboardCheck } from "react-icons/fa6";
-import { FaGraduationCap } from "react-icons/fa6";
-import { FaComments } from "react-icons/fa6";
+import { FaComments } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import { TbReport } from "react-icons/tb";
 
 
 
@@ -273,7 +274,9 @@ const Hod = () => {
                 }`}
                 onClick={() => { setActiveView('attendance'); closeMobileMenu(); }}
               >
-                <FaClipboardCheck className="w-5 h-5" />
+                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
                 <span className="font-medium">Attendance</span>
               </div>
               <div 
@@ -286,7 +289,10 @@ const Hod = () => {
                 }`}
                 onClick={() => { setActiveView('exams'); closeMobileMenu(); }}
               >
-                <FaGraduationCap className="w-5 h-5" />
+                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                </svg>
                 <span className="font-medium">Exams/Grades</span>
               </div>
               <div 
@@ -299,9 +305,7 @@ const Hod = () => {
                 }`}
                 onClick={() => { setActiveView('reports'); closeMobileMenu(); }}
               >
-                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                </svg>
+                <TbReport className="w-5 h-5" />
                 <span className="font-medium">Reports</span>
               </div>
               <div 
@@ -314,9 +318,7 @@ const Hod = () => {
                 }`}
                 onClick={() => { setActiveView('communication'); closeMobileMenu(); }}
               >
-                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                </svg>
+                <FaComments className="w-5 h-5" />
                 <span className="font-medium">Communications</span>
               </div>
               <div 
@@ -329,10 +331,7 @@ const Hod = () => {
                 }`}
                 onClick={() => { setActiveView('settings'); closeMobileMenu(); }}
               >
-                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
+                <IoMdSettings className="w-5 h-5" />
                 <span className="font-medium">Settings</span>
               </div>
             </div>
